a1a97968051ca9a9be227450dcd24851671b8017
satishphale <phalesatish.comp@mmcoe.edu.in>
2024-10-05 01:33:12 +0530

    added booking method in controller

--- src/main/java/com/bookmyshow/controller/BookingController.java
+++ src/main/java/com/bookmyshow/controller/BookingController.java
@@ -1,9 +1,15 @@
 package com.bookmyshow.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BookingController {
+
+    @PostMapping("/api/v1/book")
+    public void Booking() {
+
+    }
 
 }
