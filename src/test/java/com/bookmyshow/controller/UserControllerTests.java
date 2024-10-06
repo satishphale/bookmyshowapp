aadd3e67109faea1a045d09cad2724e037cf804b
satishphale <phalesatish.comp@mmcoe.edu.in>
2024-10-06 23:57:42 +0530

    added testcase for user controller

---
+++ src/test/java/com/bookmyshow/controller/UserControllerTests.java
@@ -0,0 +1,36 @@
+package com.bookmyshow.controller;
+
+import com.bookmyshow.domain.User;
+import com.bookmyshow.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
+public class UserControllerTests {
+
+    @Autowired
+    public MockMvc mockMvc;
+
+    @MockBean
+    UserService userService;
+
+    @Test
+    public void GetUserSuccess() throws Exception {
+        User user = new User();
+        user.setId(1L);
+        Mockito.when(userService.getUser(1L)).thenReturn(user);
+
+        mockMvc.perform(get("/users/1"))
+                .andExpect(status().isOk());
+    }
+}
