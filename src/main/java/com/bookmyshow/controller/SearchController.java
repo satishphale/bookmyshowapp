31925757f1af0724035b775594e7c6b9692193e7
satishphale <phalesatish.comp@mmcoe.edu.in>
2024-10-20 21:46:45 +0530

    Added TO-DO

--- src/main/java/com/bookmyshow/controller/SearchController.java
+++ src/main/java/com/bookmyshow/controller/SearchController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000") // instead of hardcoding tokenize url
 @RestController
 @RequestMapping("/api/v1")
 public class SearchController {
